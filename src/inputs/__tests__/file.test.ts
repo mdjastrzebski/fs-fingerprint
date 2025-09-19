6ec6f66c3c22ee9e49ba7138d86fa35e502a8bc8
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-09-19 22:48:18 +0200

    .

--- src/inputs/__tests__/file.test.ts
+++ src/inputs/__tests__/file.test.ts
@@ -6,7 +6,7 @@ import { EMPTY_HASH } from "../../constants.js";
 import type { FingerprintConfig } from "../../types.js";
 import { calculateFileHash, calculateFileHashSync } from "../file.js";
 
-const { rootDir, writeFile } = createRootDir("file-test");
+const { rootDir, prepareRootDir, writeFile } = createRootDir("file-test");
 
 const baseConfig: FingerprintConfig = {
   rootDir,
@@ -14,11 +14,7 @@ const baseConfig: FingerprintConfig = {
 };
 
 beforeEach(() => {
-  if (fs.existsSync(baseConfig.rootDir)) {
-    fs.rmSync(baseConfig.rootDir, { recursive: true });
-  }
-
-  fs.mkdirSync(baseConfig.rootDir, { recursive: true });
+  prepareRootDir();
 });
 
 describe("calculateFileHash", () => {
