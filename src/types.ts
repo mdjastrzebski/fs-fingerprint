5700bdbcaa929a0bee1cca693cd7fc876fe1775e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-09-30 07:13:26 +0200

    tweaks

--- src/types.ts
+++ src/types.ts
@@ -8,19 +8,19 @@ type StringWithAutoSuggest<T> = (string & {}) | T;
 export type HashAlgorithm = StringWithAutoSuggest<"sha1" | "sha256" | "sha512">;
 
 export interface FingerprintOptions {
-  /** Glob patterns indicating files (and directories) to include */
+  /** Glob patterns indicating files (and directories) to include (default: '**' - all) */
   files?: readonly string[];
 
-  /** Glob patterns indicating files (and directories) to ignore */
+  /** Glob patterns indicating files (and directories) to ignore (default: none) */
   ignores?: readonly string[];
 
-  /** Extra inputs to include in the fingerprint: text, json, etc */
+  /** Extra inputs to include in the fingerprint: text, json, etc (default: none) */
   contentInputs?: readonly ContentInput[];
 
-  /** Hashing algorithm to use */
+  /** Hashing algorithm to use (default: "sha1") */
   hashAlgorithm?: HashAlgorithm;
 
-  /** Whether to ignore files ignored by Git */
+  /** Whether to ignore files ignored by Git (default: false) */
   gitIgnore?: boolean;
 }
 
@@ -33,14 +33,31 @@ export interface Config {
 }
 
 export interface ContentInput {
+  /** The key to identify the content */
   key: string;
+
+  /** The content to hash */
   content: string;
+
+  /**
+   * Whether to omit the clear-text content from the fingerprint manifest.
+   * It will still be included in the fingerprint hash.
+   * (default: false)
+   * */
   secret?: boolean;
 }
 
+/**
+ * Unique fingerprint hash representing the matched files paths and content, as well as passed content inputs
+ * */
 export interface Fingerprint {
+  /** The fingerprint hash */
   hash: string;
+
+  /** Files included in the fingerprint hash */
   files: FileHash[];
+
+  /** Content included in the fingerprint hash */
   content: ContentHash[];
 }
 
@@ -52,5 +69,6 @@ export interface FileHash {
 export interface ContentHash {
   key: string;
   hash: string;
+
   content?: string;
 }
