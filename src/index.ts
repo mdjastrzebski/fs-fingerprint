97bcab1fb425f5c5931d3bd021bbf9b15db66781
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-10-07 22:15:46 -0700

    remove private exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { calculateFingerprint, calculateFingerprintSync } from "./fingerprint.js";
-export { calculateFileHash, calculateFileHashSync } from "./inputs/file.js";
-export { calculateContentHash, textContent, jsonContent, envContent } from "./inputs/content.js";
+export { textContent, jsonContent, envContent } from "./inputs/content.js";
 export { getGitIgnoredPaths } from "./git.js";
-export { hashData, mergeHashes } from "./utils.js";
 
 export type * from "./types.js";
