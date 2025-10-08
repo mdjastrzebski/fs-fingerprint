191c95d4235536205ff167a1cee16b6cf9d4f40a
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-10-07 22:26:54 -0700

    .

--- src/__tests__/fingerprint.test.ts
+++ src/__tests__/fingerprint.test.ts
@@ -1,7 +1,7 @@
 import { execSync } from "node:child_process";
 import * as fs from "node:fs";
 import * as path from "node:path";
-import { beforeEach, describe, expect, spyOn, test } from "bun:test";
+import { beforeEach, describe, expect, test } from "bun:test";
 
 import { findData, findFile } from "../../test-utils/assert.js";
 import { formatFingerprint } from "../../test-utils/format.js";
